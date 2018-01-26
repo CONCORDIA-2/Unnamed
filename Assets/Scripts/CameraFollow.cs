ff952e95fdeadf266c35ea3323952a9efff49709
Tri-Luong Steven Dien <steven.dien@outlook.com>
2018-01-25 23:27:01 -0500

    Created a "TestRealm" scene. Implemented the player movement (WASD to move around and SPACE to jump). The camera follows the player.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform mTarget;
+
+    private Vector3 mOffset;
+
+    void Start()
+    {
+        mOffset = transform.position - mTarget.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = mTarget.position + mOffset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float mMovementSpeed;
+    public float mJumpPower;
+
+    private float mRotationDegreesPerSecond = 180f;
+    private bool mGrounded = true;
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+     {
+         float horizontal = Input.GetAxis("Horizontal");
+         float vertical = Input.GetAxis("Vertical");
+        
+         Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+ 
+         if (horizontal != 0f || vertical != 0f)
+         {
+             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mRotationDegreesPerSecond * Time.deltaTime);
+             transform.position += direction* Time.deltaTime * mMovementSpeed;
+         }
+     }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown("space") && mGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, mJumpPower, 0.0f), ForceMode.Impulse);
+            mGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Jumpable")
+        {
+            mGrounded = true;
+        }
+    }
+}
