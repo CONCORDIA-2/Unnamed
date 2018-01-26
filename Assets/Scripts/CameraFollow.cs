3f4585d76b720dff8866c4cd420b859565007b31
Tri-Luong Steven Dien <steven.dien@outlook.com>
2018-01-25 23:39:19 -0500

    Added comments to the scripts.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,12 +9,16 @@ public class CameraFollow : MonoBehaviour
 
     void Start()
     {
+        // Gets the distance between the player character and the position of the main camera
         mOffset = transform.position - mTarget.position;
     }
 
     void FixedUpdate()
     {
+        // Gets the future camera position
         Vector3 targetCamPos = mTarget.position + mOffset;
+        
+        // Smoothly follow the player character
         transform.position = Vector3.Lerp(transform.position, targetCamPos, Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,23 +17,31 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Move()
-     {
-         float horizontal = Input.GetAxis("Horizontal");
-         float vertical = Input.GetAxis("Vertical");
-        
-         Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
- 
-         if (horizontal != 0f || vertical != 0f)
-         {
-             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mRotationDegreesPerSecond * Time.deltaTime);
-             transform.position += direction* Time.deltaTime * mMovementSpeed;
-         }
-     }
+    {
+        // Gets the keyboard inputs (WASD)
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Creates a vector direction from the inputs
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+
+        // If there's an input from the player
+        if (horizontal != 0f || vertical != 0f)
+        {
+            // Rotates the character so it faces the same orientation as the vector direction
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mRotationDegreesPerSecond * Time.deltaTime);
+
+            // Moves the characters towards that vector direction
+            transform.position += direction * Time.deltaTime * mMovementSpeed;
+        }
+    }
 
     private void Jump()
     {
+        // If the player has pressed the spacebar and its character is grounded
         if (Input.GetKeyDown("space") && mGrounded)
         {
+            // Makes the character jump toward the Y axis
             GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, mJumpPower, 0.0f), ForceMode.Impulse);
             mGrounded = false;
         }
@@ -41,6 +49,7 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        // Checks if the character is touching a "jumpable" object (box, terrain, etc)
         if (collision.collider.gameObject.tag == "Jumpable")
         {
             mGrounded = true;
