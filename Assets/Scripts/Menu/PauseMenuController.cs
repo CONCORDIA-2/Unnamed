ba3be10b7575cc2ab2558b2b2aba0163634f8700
Nicole Parmentier <nclparmentier@gmail.com>
2018-03-08 09:28:59 -0500

    Working on pause menu

---
+++ Assets/Scripts/Menu/PauseMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public Button resume;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause") || Input.GetKeyDown("escape"))   //start
+        {
+            if (isPaused)   //the player is trying to leave the pause menu
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        isPaused = true;
+        resume.Select();
+    }
+}
