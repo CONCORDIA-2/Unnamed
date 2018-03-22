d8c470ef5e80dd18b35f0969fddfcc7640bca954
charlesDouc <charles.douc@gmail.com>
2018-03-22 00:43:48 -0400

    Work done Wensday night.

---
+++ Assets/Scripts/ConveyorBelt/scrollTexture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollTexture : MonoBehaviour {
+
+	// public variables
+	public float m_speedScrollX = 0.5f;
+	public float m_speedScrollY = 0.5f;
+
+	// private variables
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		
+	}
+
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		float offsetX = Time.time * m_speedScrollX;
+		float offsetY = Time.time * m_speedScrollY;
+
+		GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (offsetX, offsetY);
+		
+	}
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+
+}
