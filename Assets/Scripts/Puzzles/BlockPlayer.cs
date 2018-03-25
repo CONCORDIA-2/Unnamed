a287d181aa1e35fa38d3d994443181af9993acdf
vi <reducedprocess@gmail.com>
2018-03-25 16:20:23 -0400

    make level 3 doors close, prevent rabbit from going into raven section in level 3, level 3 asset and light adjustments

---
+++ Assets/Scripts/Puzzles/BlockPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlayer : MonoBehaviour {
+
+	public bool isRaven = true;
+
+	void Start() {
+		isRaven = GameObject.FindGameObjectWithTag("PlayerManager").GetComponent<LocalPlayerManager>().IsRaven();
+	}
+
+	void OnCollisionStay(Collision collision) {
+		if (collision.gameObject.tag == "block rabbit" && isRaven) {
+			Debug.Log("collision ignored - raven");
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponents<CapsuleCollider>()[0]);
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponents<CapsuleCollider>()[1]);
+		}
+
+		if (collision.gameObject.tag == "block raven" && !isRaven) {
+			Debug.Log("collision ignored - rabbit");
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponents<CapsuleCollider>()[0]);
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponents<CapsuleCollider>()[1]);
+		}
+	}
+}
---
+++ Assets/Scripts/Puzzles/Level3LockDoor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3LockDoor : MonoBehaviour {
+
+	public GameObject door;
+    public Transform raised, lowered;
+    public float timeToMove = 1.0f;
+  
+    public bool triggered = false, used = false, moving = false;
+
+	void Update () {
+        if (triggered && !moving)
+        {
+            moving = true;
+            StartCoroutine(MoveToPosition(door.transform, lowered.position, timeToMove));
+        }
+        else if (!triggered && !moving)
+        {
+            moving = true;
+            StartCoroutine(MoveToPosition(door.transform, raised.position, timeToMove));
+        }
+    }
+
+    void OnTriggerEnter(Collider collider) {
+    	if (collider.gameObject.tag == "Player") triggered = true;
+    }
+
+    void OnTriggerExit(Collider collider) {
+    	if (collider.gameObject.tag == "Player") triggered = false;
+    }
+
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+    {
+        var currentPos = door.transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            door.transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+        moving = false;
+    }
+}
