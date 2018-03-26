5da024f081aae907ec6a8c97175029bf8eaa8bba
Tri-Luong Steven Dien <steven.dien@outlook.com>
2018-03-26 00:46:43 -0400

    Added the tickbot prefabs (with animation). Added the Rabbit incap walking anim.

---
+++ Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class NetworkController : NetworkManager
+{
+    public int mPlayerNumber;
+    private int mSpawnNumber = 0;
+    public Transform[] spawnPositions;
+
+    //Called on client when connect
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        // Create message to set the player
+        IntegerMessage msg = new IntegerMessage(mPlayerNumber);
+
+        // Call Add player and pass the message
+        ClientScene.AddPlayer(conn, 0, msg);
+    }
+
+    // Server
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
+    {
+        // Read client message and receive index
+        if (extraMessageReader.Length > 0)
+        {
+            var stream = extraMessageReader.ReadMessage<IntegerMessage>();
+            mPlayerNumber = stream.value;
+        }
+
+        //Select the prefab from the spawnable objects list
+        var playerPrefab = spawnPrefabs[mPlayerNumber];
+
+        // Create player object with prefab
+        var player = Instantiate(playerPrefab, spawnPositions[mSpawnNumber].position, Quaternion.identity) as GameObject;
+
+        // Add player object for connection
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    public void SetPlayerNumber(int number)
+    {
+        mPlayerNumber = number;
+    }
+}
